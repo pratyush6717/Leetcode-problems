0cffc9fa7982233987cf8d03a62d522f0a065fad
pratyush <41910846+pratyush6717@users.noreply.github.com>
2022-01-24 23:52:01 +0530

    Time: 377 ms (60.79%), Space: 8 MB (60.83%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>&board,int i,int j,int count,string word){
-        // if the count and word.length are same which means we have covered the string and there is nothing left to search so ... we will return true...
-        if(count==word.length())return true;
-        
-        // now we will take take of our recurssion going out of bound ...
-        // so 
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[count]){
-            return false;
-        }
-        // we also need to take care that we are not using the same word twice.. 
-        // so we will add a blank space to whichever word we traverse
-        // we also need to fill it back because iske jo dfs dusra word k liye hoga usko toh chaiye hoga na words 
-        char temp=board[i][j];
-        board[i][j]=' ';
-        
-        bool search=dfs(board,i-1,j,count+1,word) || dfs(board,i+1,j,count+1,word) ||dfs(board,i,j-1,count+1,word) || dfs(board,i,j+1,count+1,word);
-        
-        board[i][j]=temp;
-        return search;
-    }
     bool exist(vector<vector<char>>& board, string word) {
-     //we will go through the board and find the first letter of the word in the board and then we will start the dfs search for the rest of the word left in the string word 
-        for(int i=0; i<board.size(); i++){
-            for(int j=0; j<board[i].size(); j++){
-                if(board[i][j]==word[0] && dfs(board,i,j,0,word)){
+        for(int i = 0 ; i < board.size() ; ++i)
+           for(int j = 0 ; j < board[0].size() ; ++j)
+               if(dfs(0 , i , j , board , word))
                    return true;
-                }
-            }
-        }
-        return false;   
+       return false;
     }
+    bool dfs(int index , int x , int y , vector<vector<char>> &board , string &word)
+   {
+       if(index == word.size()) // word exists in matrix
+           return true;
+       
+       if(x < 0 or y < 0 or x >= board.size() or y >= board[0].size() or board[x][y] == '.') // boundary check + visited check
+           return false;
+       
+       if(board[x][y] != word[index])
+           return false;
+       
+       char temp = board[x][y];
+       board[x][y] = '.'; // marking it visited
+       
+       // Move in 4 directions[UP , DOWN , LEFT , RIGHT]
+       if(dfs(index + 1 , x - 1 , y , board , word) or dfs(index + 1 , x + 1 , y , board , word) or dfs(index + 1 , x , y - 1 , board , word) or dfs(index + 1 , x , y + 1 , board , word))
+           return true;
+           
+       board[x][y] = temp; // backtrack step
+       return false;
+       
+   }
 };
