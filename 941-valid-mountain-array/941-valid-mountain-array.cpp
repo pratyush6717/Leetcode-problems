6853ef6fc3557553a0947c87add4ee5bcd0f7fa2
pratyush <41910846+pratyush6717@users.noreply.github.com>
2022-01-25 15:18:07 +0530

    Time: 72 ms (5.17%), Space: 22.5 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        vector<int>increasingarray;
-        vector<int>decreasingarray;
+       // vector<int>increasingarray;
+       // vector<int>decreasingarray;
         if(arr.size()<3){
             return false;
         }
@@ -10,22 +10,27 @@ public:
         if(arr[1]<arr[0]){
             return false;
         }
-        
+        int count =0;
         for( i=0;i<arr.size()-1;i++){
             
-            cout<<arr[i]<<" ";
-            increasingarray.push_back(arr[i]);
+           // cout<<arr[i]<<" ";
+            //increasingarray.push_back(arr[i]);
             if(arr[i+1]<=arr[i]){
+                count++;
                 break;
             }
         }
         for( j=i;j<arr.size()-1;j++){
             if(arr[j+1]>=arr[j]){
+                count++;
                 break;
             }
-            decreasingarray.push_back(arr[j]);
+            //decreasingarray.push_back(arr[j]);
         }
-        if(increasingarray.size()+decreasingarray.size()==arr.size()){
+        // if(increasingarray.size()+decreasingarray.size()==arr.size()){
+        //     return true;
+        // }
+        if(count==1){
             return true;
         }
         return false;
