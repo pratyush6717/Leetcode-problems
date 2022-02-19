ec1082fd11b1999b69f99e16c8f92cf9f5914c17
pratyush <41910846+pratyush6717@users.noreply.github.com>
2022-02-19 21:51:36 +0530

    Added solution - LeetHub

--- Steps by Knight - GFG/steps-by-knight.cpp
+++ Steps by Knight - GFG/steps-by-knight.cpp
@@ -8,77 +8,44 @@ class Solution
 {
     public:
     //Function to find out minimum steps Knight needs to reach target position.
-	int minStepToReachTarget(vector<int>&KnightPos,vector<int>&TargetPos,int n)
+	int minStepToReachTarget(vector<int>&Kpos,vector<int>&Tpos,int N)
 	{
 	    // Code here
-    int x1=KnightPos[0];
-    int y1=KnightPos[1];
-    int x2=TargetPos[0];
-    int y2=TargetPos[1];
-    
-    if(x1==x2 and y1==y2)   return 0;
-    
-    
-    int a[1001][1001];
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            a[i][j]=0;
-        }
-    }
-    queue<pair<int,int>>q;
-    q.push({x1-1,y1-1});
-    while(!q.empty())
-    {
-        auto cur=q.front();
-        int i=cur.first;
-        int j=cur.second;
-        q.pop();
-    //  dx[] = { -1, 1, 2 -2, -1, 1, 2, -2};
-    //  dy[] = {  2, 2, 1 ,-1, -2, -2, -1, 1};
-        if(i-1>=0 and j+2<n and a[i-1][j+2]==0)
-        {
-            a[i-1][j+2]=a[i][j]+1;
-            q.push({i-1,j+2});
-        }
-        if( i+1<n and j+2<n and a[i+1][j+2]==0)
-        {
-            a[i+1][j+2]=a[i][j]+1;
-            q.push({i+1,j+2});
-        }
-        if(i-1>=0 and j-2>=0  and a[i-1][j-2]==0)
-        {
-            a[i-1][j-2]=a[i][j]+1;
-            q.push({i-1,j-2});
-        }
-        if( j-2>=0 and i+1<n and a[i+1][j-2]==0)
-        {
-            a[i+1][j-2]=a[i][j]+1;
-            q.push({i+1,j-2});
-        }
-        if(i-2>=0 and j+1<n and a[i-2][j+1]==0)
-        {
-            a[i-2][j+1]=a[i][j]+1;
-            q.push({i-2,j+1});
-        }
-        if(i-2>=0 and j-1>=0 and a[i-2][j-1]==0)
-        {
-            a[i-2][j-1]=a[i][j]+1;
-            q.push({i-2,j-1});
-        }
-        if( i+2<n and j+1<n and a[i+2][j+1]==0)
-        {
-            a[i+2][j+1]=a[i][j]+1;
-            q.push({i+2,j+1});
-        }
-        if( j-1>=0 and i+2<n and a[i+2][j-1]==0)
-        {
-            a[i+2][j-1]=a[i][j]+1;
-            q.push({i+2,j-1});
-        }
-    }
-    return a[x2-1][y2-1];
+	    int dx[8] ={-1,1,-1,1,2,-2,2,-2};
+	    int dy[8] ={-2,-2,2,2,-1,-1,1,1};
+	    
+	    queue<pair<int,int>> q;
+	    q.push({Kpos[0] , Kpos[1]});
+	    
+	    int min_steps = 0;
+	    
+	    int vis[N+1][N+1];
+	    memset(vis , 0 , sizeof(vis));
+	    vis[Kpos[0]][Kpos[1]] = 1;
+	    
+	    while(!q.empty())
+	    {
+	        int sz = q.size();
+	        while(sz--)
+	        {
+	            int x = q.front().first , y = q.front().second;
+	            q.pop();
+	            
+	            if(x == Tpos[0] && y == Tpos[1])return min_steps;
+	            
+    	        for(int i = 0; i < 8; i++)
+    	        {
+    	            int nx = x+dx[i] , ny = y+dy[i];
+    	            
+    	            if(nx < 0 || ny < 0 || nx > N || ny > N || vis[nx][ny] == 1)continue;
+    	            vis[nx][ny] = 1;
+    	            q.push({nx , ny});
+    	        }
+	        }
+	        min_steps++;
+	    }
+	    
+	    return min_steps;
 	}
 };
 
