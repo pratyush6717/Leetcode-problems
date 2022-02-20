41349071974faaa6cdcab49d1fc868c3abf2aa87
pratyush <41910846+pratyush6717@users.noreply.github.com>
2022-02-20 22:27:45 +0530

    Added solution - LeetHub

---
+++ Topological sort - GFG/topological-sort.cpp
@@ -0,0 +1,86 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	void findtoposort(int node,vector<int>& vis,stack<int>& st,vector<int>adj[]){
+	    vis[node]=1;
+	    for(auto it: adj[node]){
+	        if(!vis[it]){
+	            findtoposort(it,vis,st,adj);
+	        }
+	    }
+	    st.push(node);
+	}
+	vector<int> topoSort(int v, vector<int> adj[]) 
+	{
+	    // code here
+	    stack<int>st;
+	    vector<int>vis(v,0);
+	    for(int i=0;i<v;i++){
+	        if(vis[i]==0){
+	            findtoposort(i,vis,st,adj);
+	        }
+	    }
+	    vector<int>res;
+	    while(!st.empty()){
+	        int x=st.top();
+	        st.pop();
+	        res.push_back(x);
+	    }
+	    return res;
+	}
+};
+
+// { Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    
+    if(V!=res.size())
+    return 0;
+    
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}  // } Driver Code Ends
