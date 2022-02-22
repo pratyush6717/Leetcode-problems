f58d1286b60f09ff5e614811654101ba105d2d92
pratyush <41910846+pratyush6717@users.noreply.github.com>
2022-02-22 12:56:51 +0530

    Time: 12 ms (62.12%), Space: 14.3 MB (28.77%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        //vector<int>bfs;
+        
+        vector<vector<int>>vis(image.size(),vector<int>(image[0].size(),0));
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        vis[sr][sc]=1;
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int i=it.first;
+            int j=it.second;
+            if(i-1>=0&&image[i-1][j]==image[i][j]&&vis[i-1][j]==0){
+                vis[i-1][j]=1;
+                q.push({i-1,j});
+            }
+            if(j-1>=0&&image[i][j-1]==image[i][j]&&vis[i][j-1]==0){
+                vis[i][j-1]=1;
+                q.push({i,j-1});
+            }
+            if(i+1<image.size()&&image[i+1][j]==image[i][j]&&vis[i+1][j]==0){
+                vis[i+1][j]=1;
+                q.push({i+1,j});
+            }
+            if(j+1<image[0].size()&&image[i][j+1]==image[i][j]&&vis[i][j+1]==0){
+                vis[i][j+1]=1;
+                q.push({i,j+1});
+            }
+            image[i][j]=newColor;
+        }
+        return image;
+    }
+};
