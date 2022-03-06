f6eeae0585ec4e40b8d3fd3d0d11321642929df8
Pratyush Kumar <pratyushsingh6710@gmail.com>
2022-03-06 21:39:22 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    bool isvalid(int x, int y, int row, int col){
+       if(x<0 || y<0 || x>=row || y>=col) return false;
+       return true;
+   }
+    int orangesRotting(vector<vector<int>>& grid) {
+        // Code here
+        int n=grid.size();
+        int m=grid[0].size();
+        int fresh=0,time=0;
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
+                else if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
+        }
+        
+        int dx[4]={0, 1, 0, -1};
+        int dy[4]={1, 0, -1, 0};
+        
+        while(!q.empty()){
+            time++;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int x=q.front().first;
+                int y=q.front().second;
+                q.pop();
+                for(int j=0;j<4;j++){
+                    int xdx=x+dx[j];
+                    int ydy=y+dy[j];
+                    if(isvalid(xdx,ydy,n,m)&&grid[xdx][ydy]==1){
+                        grid[xdx][ydy]=2;
+                        fresh--;
+                        q.push({xdx,ydy});
+                    }
+                }
+            }
+            if(fresh==0)
+                return time;
+            
+            
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
