b86551613456ea5f2703fabe1f553a58752d4f63
Pratyush Kumar <pratyushsingh6710@gmail.com>
2022-03-16 14:01:59 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -12,24 +12,23 @@ class Solution
     { 
        // Your code here
        int t[n+1][W+1];
-   memset (t,-1,sizeof(t));
-   for(int i=0;i<=n;i++){
-       t[i][0]=0;
-   }
-   for(int i=0;i<=W;i++){
-       t[0][i]=0;
-   }
-   for(int i=1;i<=n;i++){
-       for(int j=1;j<=W;j++){
-   if(wt[i-1]<=j){
-        t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
-   }
-   else{
-       t[i][j]=t[i-1][j];
-   }
+       for(int i=0;i<=n;i++){
+           t[i][0]=0;
        }
-   }
-   return t[n][W];
+       for(int j=0;j<=W;j++){
+           t[0][j]=0;
+       }
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=W;j++){
+               if(wt[i-1]<=j){
+                   t[i][j]=max(val[i-1]+t[i-1][j-wt[i-1]],t[i-1][j]);
+               }
+               else{
+                   t[i][j]=t[i-1][j];
+               }
+           }
+       }
+       return t[n][W];
     }
 };
 
