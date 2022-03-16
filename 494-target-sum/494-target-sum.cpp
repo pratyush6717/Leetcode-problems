0938f1f20244390b8646e3f4747f67ac5a97c8a3
Pratyush Kumar <pratyushsingh6710@gmail.com>
2022-03-16 20:57:49 +0530

    Time: 16 ms (69.54%), Space: 9.2 MB (53.39%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -14,24 +14,17 @@ public:
         }
         target=(sum+val)/2;
         int t[n+1][target+1];
-        int k=1;
-        t[0][0] = 1;
         for(int i=0;i<=n;i++){
             for(int j=0;j<=target;j++){
-                if (i == 0 && j > 0)
+                if (i == 0)
 				t[i][j] = 0;
-			if (j == 0 && i > 0) {
-				if (nums[i - 1] == 0) {
-					t[i][j] = pow(2, k);
-					k++;
-				}
-				else
-					t[i][j] = t[i - 1][j];
-			}
+			if (j == 0 ) {
+				t[i][j] = 1;
             }
         }
+    }
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=target;j++){
+            for(int j=0;j<=target;j++){
                 if(nums[i-1]<=j){
                     t[i][j]=t[i-1][j]+t[i-1][j-nums[i-1]];
                 }
