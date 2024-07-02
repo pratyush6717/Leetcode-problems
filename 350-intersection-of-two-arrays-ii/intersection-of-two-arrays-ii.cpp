926a95d4c5338d5665268450246ac54abb01b7d7
Pratyush Kumar <pratyushsingh6710@gmail.com>
2024-07-03 00:08:54 +0530

    Time: 5 ms (38.58%) | Memory: 12.5 MB (91.17%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -4,26 +4,18 @@ public:
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         vector<int> res;
-        map<int,bool>vis;
-        map<int,int>mp;
-        map<int,int>mp1;
-        for(int i=0;i<nums1.size();i++){
-            vis[nums1[i]]=true;
-            mp[nums1[i]]++;
-        }
-        for(int i=0;i<nums2.size();i++){
-            mp1[nums2[i]]++;
-        }
-         for(int i=0;i<nums2.size();i++){
-            if(vis[nums2[i]]==true){
-                int x=mp[nums2[i]];
-                int y=mp1[nums2[i]];
-                x= min(x,y);
-                while(x>0){
-                res.push_back(nums2[i]);
-                x--;
-                }
-                vis[nums2[i]]=false;
+        int i=0,j=0;
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
             }
         }
         return res;
